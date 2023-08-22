d0c4f08c72ade0f2d7c02d4f906e8c91560e7b59
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-08-22 10:49:12 +0700

    Add user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,21 +18,61 @@ class UserSeeder extends Seeder
     {
         $admin = User::create([
             'name' => 'Admin',
-            'email' => 'admin@example.com',
+            'email' => 'admin@gmail.com',
             'password' => bcrypt('12345678'),
             'created_at' => Carbon::now()
         ]);
 
         $admin->assignRole('admin');
 
-        $user = User::create([
-            'name' => 'User',
-            'address' => 'Surakarta',
-            'email' => 'user@example.com',
-            'password' => bcrypt('12345678'),
-            'created_at' => Carbon::now()
-        ]);
+        $users = [
+            [
+                'name' => 'Ihsan Dwi Asani',
+                'address' => 'Jlumbang RT 02/05',
+                'email' => 'ihsan@gmail.com',
+                'password' => bcrypt('12345678'),
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Darsih',
+                'address' => 'Nayan RT 02/06 ',
+                'email' => 'darsih@gmail.com',
+                'password' => bcrypt('12345678'),
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Eni Astuti',
+                'address' => 'Ngentak',
+                'email' => 'eni@@gmail.com',
+                'password' => bcrypt('12345678'),
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Tri Hastuti',
+                'address' => 'Bugel ',
+                'email' => 'tri@gmail.com',
+                'password' => bcrypt('12345678'),
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Ambarwati',
+                'address' => 'Banaran RT 03/03',
+                'email' => 'ambarwati@gmail.com',
+                'password' => bcrypt('12345678'),
+                'created_at' => Carbon::now()
+            ],
+        ];
 
-        $user->assignRole('user');
+        foreach ($users as $user) {
+            $user = User::create([
+                'name' => $user['name'],
+                'address' => $user['address'],
+                'email' => $user['email'],
+                'password' => bcrypt('12345678'),
+                'created_at' => Carbon::now()
+            ]);
+
+            $user->assignRole('user');
+        }
     }
 }
