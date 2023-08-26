eeeaabacc7eb1ff87e350c52de4c4f2eafd22ab2
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-08-26 11:49:53 +0700

    Update model

--- app/Models/disease.php
+++ app/Models/disease.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Disease extends Model
 {
     use HasFactory;
+
+    protected $table = 'diseases';
+
+    protected $guarded = [];
 }
--- app/Models/prevention.php
+++ app/Models/prevention.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Prevention extends Model
 {
     use HasFactory;
+
+    protected $table = 'preventions';
+
+    protected $guarded = [];
+
+    public function disease()
+    {
+        return $this->belongsTo(User::class, 'disease_id');
+    }
 }
